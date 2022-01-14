2167afdace92fa2e90ff7939fdbb61f5dc0195d7
Luiz Felipe Burgatt Jolo <luiz.jolo@grupopereira.local>
2022-01-14 16:00:28 -0400

    Projeto inicial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button color="danger">Danger</Button>
     </div>
   );
 }
